5e92fa6bf435b919a49907a7ff6c6526930f00e7
flagarde <f.lagarde@ipnl.in2p3.fr>
2018-04-24 18:50:21 +0200

    Try

--- src/Processors/Streamout/include/SDHCAL_DataFormat.h
+++ src/Processors/Streamout/include/SDHCAL_DataFormat.h
@@ -9,8 +9,8 @@ public:
     virtual ~SDHCAL_DataFormat(){}
     virtual void Parse()
     {
-      RawBuffer->getBuffer().printBuffer();
-      /*SDHCAL_RawBuffer_Navigator bufferNavigator(RawBuffer->getBuffer(),_BitsToSkip);
+      //RawBuffer->getBuffer().printBuffer();
+      SDHCAL_RawBuffer_Navigator bufferNavigator(RawBuffer->getBuffer(),_BitsToSkip);
       uint32_t idstart = bufferNavigator.getStartOfDIF();
       //_DIFStarter[idstart]++;
       if (!bufferNavigator.validBuffer()) return;
@@ -76,6 +76,6 @@ public:
       //_SizeAfterAllData[eod.getsize()]++;
       //int nonzeroCount = 0;
       //for (uint8_t *it = eod.buffer(); it != eod.endOfBuffer(); it++)if (int(*it) != 0)nonzeroCount++;
-      //_NonZeroValusAtEndOfData[nonzeroCount]++;*/
+      //_NonZeroValusAtEndOfData[nonzeroCount]++;
     }
 };
