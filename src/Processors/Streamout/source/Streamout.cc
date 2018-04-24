0a5b251fe1eb555c09e75c3af3c029af92368923
flagarde <f.lagarde@ipnl.in2p3.fr>
2018-04-24 19:56:12 +0200

    Cleaning

--- src/Processors/Streamout/source/Streamout.cc
+++ src/Processors/Streamout/source/Streamout.cc
@@ -21,11 +21,9 @@
 #include "IMPL/LCEventImpl.h"
 #include "IMPL/CalorimeterHitImpl.h"
 #include <IMPL/LCRunHeaderImpl.h>
-//#include "DIFUnpacker.h"
 #include <sstream>
 #include <set>
 #include <limits>
-//#include "DataFormat.h"
 using namespace lcio;
 
 Streamout aStreamout;
@@ -103,87 +101,9 @@ void Streamout::processEvent(LCEvent *evt)
     _CollectionSizeCounter[nElement]++;
     for (int iel = 0; iel < nElement; iel++) 
     {
-      //std::cout<<iel<<std::endl;
       EVENT::LCObject* obj=col->getElementAt(iel);
       parseDataFormat->SetRawBuffer(obj);
       parseDataFormat->Parse();
-    
-      /*LMGeneric *lmobj = (LMGeneric *)(col->getElementAt(iel));
-      if (lmobj == nullptr) 
-      {
-        _nWrongObj++;
-        continue;
-      }
-      
-      std::cout<<red<<"toto"<<normal<<std::endl;
-      lmobj->getBuffer().printBuffer();
-      _nProcessedObject++;
-      SDHCAL_RawBuffer_Navigator bufferNavigator(lmobj->getBuffer(),_BitsToSkip);
-      uint32_t idstart = bufferNavigator.getStartOfDIF();
-      _DIFStarter[idstart]++;
-      if (!bufferNavigator.validBuffer()) continue;
-      DIFPtr *d = bufferNavigator.getDIFPtr();
-      if (d != nullptr) _DIFPtrValueAtReturnedPos[bufferNavigator.getDIFBufferStart()[d->getGetFramePtrReturn()]]++;
-      _SizeAfterDIFPtr[bufferNavigator.getSizeAfterDIFPtr()]++;
-      // create RawCalorimeterHit
-      int loop=0;
-      unsigned int FrameBCIDold=std::numeric_limits<unsigned int>::max();
-      for (uint32_t i = 0; i < d->getNumberOfFrames(); i++) 
-      {
-        unsigned int nbr_hit_in_asic = 0;
-        for (uint32_t j = 0; j < 64; j++) if (d->getFrameLevel(i, j, 0) || d->getFrameLevel(i, j, 1)) nbr_hit_in_asic++;
-        if (!(nbr_hit_in_asic == 64 && virer_full_asic == true)) 
-        {
-          for (uint32_t j = 0; j < 64; j++) 
-          {
-            if (!(d->getFrameLevel(i, j, 0) || d->getFrameLevel(i, j, 1)))  continue;
-            IMPL::RawCalorimeterHitImpl *hit =new IMPL::RawCalorimeterHitImpl();
-            if(hit==nullptr) continue;
-            cd["DIF_Id"]=(int)d->getID();
-            cd["Asic_Id"]=(int)d->getFrameAsicHeader(i);
-            bitset<6> Channel(j);
-            cd["Channel"]=(int)(Channel.to_ulong());
-            cd["BarrelEndcapModule"]=0;
-            cd.setCellID( hit ) ;
-            bitset<2> ThStatus;
-            ThStatus[0] = d->getFrameLevel(i, j, 0);
-            ThStatus[1] = d->getFrameLevel(i, j, 1);
-            hit->setCellID1(d->getFrameBCID(i));
-            hit->setAmplitude(ThStatus.to_ulong());
-            unsigned int TTT = (unsigned int)(d->getFrameTimeToTrigger(i));
-            if(FrameBCIDold-d->getFrameBCID(i)<0)
-            {
-              loop++;
-              TTT=(unsigned int)(loop*16777215+d->getFrameTimeToTrigger(i));
-              std::cout<<"oups"<<std::endl;
-            }
-            FrameBCIDold=d->getFrameBCID(i);
-            hit->setTimeStamp(TTT);		      		
-            RawVec->addElement(hit);
-          } // for (uint32_t j=0;j<64;j++)
-        }
-          // std::cout<<yellow<<yyy<<normal<<std::endl;
-      } // for (uint32_t i=0;i<d->getNumberOfFrames();i++)
-      // register Triggers'time : lots of values here ?
-      lcio::IntVec trig(9);
-      trig[0] = d->getDTC();
-      trig[1] = d->getGTC();
-      trig[2] = d->getBCID();
-      trig[3] = d->getAbsoluteBCID() & 0xFFFFFF;
-      trig[4] = (d->getAbsoluteBCID() / (0xFFFFFF + 1)) & 0xFFFFFF;
-      // std::cout<<red<<d->getAbsoluteBCID()<<normal<<std::endl;
-      trig[5] = d->getTASU1(); // what happen if no temperature ?
-      trig[6] = d->getTASU2();
-      trig[7] = d->getTDIF();
-      std::string ss = "DIF" + std::to_string(d->getID()) + "_Triggers";
-      RawVec->parameters().setValues(ss, trig);
-      if (bufferNavigator.hasSlowControlData()) _hasSlowControl++;
-      if (bufferNavigator.badSCData()) _hasBadSlowControl++;
-      Buffer eod = bufferNavigator.getEndOfAllData();
-      _SizeAfterAllData[eod.getsize()]++;
-      int nonzeroCount = 0;
-      for (uint8_t *it = eod.buffer(); it != eod.endOfBuffer(); it++)if (int(*it) != 0)nonzeroCount++;
-      _NonZeroValusAtEndOfData[nonzeroCount]++;*/
     } // for (int iel=0; iel<nElement; iel++)
   } 
   catch (DataNotAvailableException &e) 
@@ -217,5 +137,5 @@ void Streamout::end()
   std::cout << "Number of Slow Control found " << _hasSlowControl<< " out of which " << _hasBadSlowControl << " are bad"<< std::endl;
   printCounter("Size remaining after all of data have been processed",_SizeAfterAllData);
   printCounter("Number on non zero values in end of data buffer",_NonZeroValusAtEndOfData);
-  //delete parseDataFormat;
+  delete parseDataFormat;
 }
